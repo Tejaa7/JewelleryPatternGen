c2c513122783909f29654481c5d5cd8cea3afb77
Buvesh <buveshb598@gmail.com>
2024-09-26 16:26:36 +0530

    added files to views and images

--- WEBSITE/api/index.js
+++ WEBSITE/api/index.js
@@ -7,6 +7,24 @@ app.set('views', path.join(__dirname, '..', 'views'));
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');  
+app.get('/Home',(req,res)=>{
+    res.render('Home.ejs')
+})
+app.get('/login',(req,res)=>{
+    res.render('login.ejs')
+})
+app.get('/signup',(req,res)=>{
+    res.render('signup.ejs')
+})
+app.get('/modern',(req,res)=>{
+    res.render('modern.ejs')
+})
+app.get('/traditional',(req,res)=>{
+    res.render('traditional.ejs')
+})
+app.get('/upload',(req,res)=>{
+    res.render('upload.ejs')
+})
 app.get('/contactus',(req,res)=>{
     res.render('contactus.ejs')
 })
